8cabf46b2aa05e33a14d15caffcfdb550a9e48a6
Neto test <akmalova.elza@yandex.ru>
2023-11-09 20:41:25 +0300

    Main changed

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,18 @@ public class Main {
     public static void main(String[] args) {
 
         int start = 200;
-        int add = 1001;
-        int amount;
-        int bonus;
+        int add = 1500;
+
 
         boolean bonus1 = add > 1000;
         if (bonus1) {
-            bonus = add / 100;
-            amount = start + add + bonus;
+            int bonus = add / 100;
+            int amount = start + add + bonus;
             System.out.println("Бонус =" + bonus);
             System.out.println("Итоговая сумма =" + amount);
         } else {
-            bonus = 0;
-            amount = start + add;
+            int bonus = 0;
+            int amount = start + add;
             System.out.println("Бонус =" + 0);
             System.out.println("Итоговая сумма =" + amount);
 
